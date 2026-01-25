9f2ca647d29d2071adb918d5075678e5a3e6f47d
wunschradio <39115843+wunschradio@users.noreply.github.com>
2026-01-25 15:39:27 +0100

    Update view.php Fix for YOOtheme Pro 5

--- administrator/components/com_k2/views/view.php
+++ administrator/components/com_k2/views/view.php
@@ -6,29 +6,92 @@
  * @copyright  Copyright (c) 2006 - 2022 JoomlaWorks Ltd. All rights reserved.
  * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
  */
-
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\View\HtmlView;
+use Joomla\Event\Event;
 
 jimport('joomla.application.component.view');
 
+if (!class_exists('K2LoadTemplateEvent')) {
+    class K2LoadTemplateEvent extends Event
+    {
+        public function __construct($view, $tpl = null)
+        {
+            $app   = Factory::getApplication();
+            $input = $app->input;
+
+            $option = $input->getCmd('option', '');
+            $vname  = $input->getCmd('view', '');
+            $layout = $input->getCmd('layout', '');
+
+            $context = trim($option . '.' . $vname, '.');
+
+            parent::__construct('onLoadTemplate', [
+                'view'    => $view,
+                'tpl'     => $tpl,
+                'context' => $context,
+                'layout'  => $layout,
+                'option'  => $option,
+                'vname'   => $vname,
+            ]);
+        }
+
+        public function __call($name, $arguments)
+        {
+            if (strpos($name, 'get') === 0 && strlen($name) > 3) {
+                $key = lcfirst(substr($name, 3));
+                return $this->getArgument($key);
+            }
+
+            throw new \BadMethodCallException(sprintf('Call to undefined method %s::%s()', static::class, $name));
+        }
+    }
+}
+
 class K2View extends HtmlView
 {
+
+    public function _addPath($type, $path)
+    {
+        $type = (string) $type;
+        $path = rtrim((string) $path, DIRECTORY_SEPARATOR);
+
+        if ($path === '') {
+            return;
+        }
+
+        if (!isset($this->_path)) {
+            $this->_path = [];
+        }
+
+        if (!isset($this->_path[$type]) || !is_array($this->_path[$type])) {
+            $this->_path[$type] = [];
+        }
+
+        if (in_array($path, $this->_path[$type], true)) {
+            return;
+        }
+
+        array_unshift($this->_path[$type], $path);
+    }
+
     public function display($tpl = null)
     {
-        // Allow for YOOtheme PRO Integration
-        $app = \Joomla\CMS\Factory::getApplication();
+        $app = Factory::getApplication();
 
-        // Only in YOOtheme PRO
         if ($app->isClient('site') && stripos($app->getTemplate(), 'yootheme') === 0) {
 
-            // Trigger the custom YOOtheme Pro event
-            $app->triggerEvent('onLoadTemplate', [$this, $tpl]);
+            $dispatcher = $app->getDispatcher();
+            if ($dispatcher && method_exists($dispatcher, 'dispatch')) {
+                $event = new K2LoadTemplateEvent($this, $tpl);
+                $dispatcher->dispatch('onLoadTemplate', $event);
+            } else {
+                $app->triggerEvent('onLoadTemplate', [$this, $tpl]);
+            }
 
-            // If the event overrode the output, print that output and don't display anything else
-            if ($this->_output) {
+            if (!empty($this->_output)) {
                 echo $this->_output;
                 return;
             }
